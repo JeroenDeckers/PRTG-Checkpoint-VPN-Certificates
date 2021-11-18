61d171cab3e3859791c11d0d0db1a5e2ff33ecae
JeroenDeckers <71337829+JeroenDeckers@users.noreply.github.com>
2021-11-18 10:21:56 +0100

    Update vpn_certificates.sh
    Added custom unit days

--- vpn_certificates.sh
+++ vpn_certificates.sh
@@ -1,7 +1,22 @@
-echo "# PRTG-Checkpoint-VPN-Certificates" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/JeroenDeckers/PRTG-Checkpoint-VPN-Certificates.git
-git push -u origin main
+#! /bin/bash
+echo "<prtg>"
+gateways=($(cpca_client lscert -stat Valid -kind IKE | egrep 'Subject' | cut -d '=' -f 3 | cut -d ' ' -f 1))
+for i in "${gateways[@]}"
+do (
+        echo "<result>"
+        echo "<channel>"
+        echo $i
+        echo "</channel>"
+        expiry_date=$(cpca_client search $i -stat Valid -kind IKE | grep Not_After | cut -d ':' -f 5,6,7)
+        remaining_days=$((($(date +%s --date "$expiry_date")-$(date +%s))/(3600*24)))
+        echo "<value>"
+        echo $remaining_days
+        echo "</value>"
+        echo "<LimitMode>1</LimitMode>"
+        echo "<LimitMinWarning>14</LimitMinWarning>"
+        echo "<LimitMinError>7</LimitMinError>"
+        echo "<CustomUnit>Days</CustomUnit>"
+        echo "</result>"
+)
+done
+echo "</prtg>"
